3db0446b167535986c52480c54f45181dac75576
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-01-25 17:57:39 -0800

    added tests for ball, strike, and foul count changes

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -3,34 +3,62 @@ import Display from './Display'
 import './App.css';
 import Dashboard from './Dashboard';
 
+export const getNextCount = (balls, strikes) => {
+  let newCount;
+  if (strikes >= 3 || balls >= 4) {
+    newCount = 0;
+  } else {
+    newCount = balls + 1;
+  }
+  return newCount
+}
+
+export const getNextFoulCount = (strikes) => {
+  let newStrike;
+  if (strikes === 0) {
+    newStrike = 1;
+} 
+else if (strikes === 1) {
+    newStrike = strikes + 2;
+} else if (strikes ===2) {
+    newStrike= 2;
+}
+return newStrike
+}
+
 function App() {
 
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
 
   const ballCount = () => {
-    if (strikes >= 3 || balls >= 4) {
-      setBalls(0);
-  } else {
-    setBalls(balls + 1)
-  }};
+    const newBallCount = getNextCount(balls, strikes);
+    setBalls(newBallCount);}
+  //   if (strikes >= 3 || balls >= 4) {
+  //     setBalls(0);
+  // } else {
+  //   setBalls(balls + 1)
+  // }};
 
   const strikeCount = () => {
-    if (strikes >= 3 || balls >= 4) {
-      setStrikes(0);
-  } else {
-    setStrikes(strikes + 1);
-  }};
+    const newStrikeCount = getNextCount(balls,strikes);
+    setStrikes(newStrikeCount);}
+  //   if (strikes >= 3 || balls >= 4) {
+  //     setStrikes(0);
+  // } else {
+  //   setStrikes(strikes + 1);
+  // }};
 
-    const foulCount = () => {
-      if (strikes === 0) {
-        setStrikes(1)
-    } 
-    else if (strikes === 1) {
-        setStrikes(strikes + 2)
-    } else if (strikes ===2) {
-        setStrikes(2)
-    }}
+  const foulCount = () => {
+    getNextFoulCount(strikes);}
+    //   if (strikes === 0) {
+    //     setStrikes(1)
+    // } 
+    // else if (strikes === 1) {
+    //     setStrikes(strikes + 2)
+    // } else if (strikes ===2) {
+    //     setStrikes(2)
+    // }}
 
     const hitCount = () => {
         setStrikes(0);
--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import ReactDom from "react-dom";
 import { render } from '@testing-library/react';
-import App from './App';
+import App, {getNextCount, getNextFoulCount} from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("rtl renders without crashing", () => {
+  render(<App />);
 });
+
+test("ballcount adds 1", () => {
+  const expected = 0
+  const balls = 4
+  const strikes = 0
+
+  const actual = getNextCount(balls, strikes);
+
+  expect(actual).toBe(expected);
+});
+
+test("foul adds count", () => {
+  const strikes = 1
+  const expected = 3
+
+  const actual = getNextFoulCount(strikes);
+
+  expect(actual).toBe(expected);
+})
